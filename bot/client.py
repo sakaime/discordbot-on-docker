77a4759876789ec8d13eab49b9b9043f684713f6
sakaime <sakame2@gmail.com>
2021-07-11 18:46:29 +0000

    add sample code

--- bot/client.py
+++ bot/client.py
@@ -1,6 +1,7 @@
 from discord.ext import commands
 
 import os
+import textwrap
 
 token_file_path = os.environ.get('DISCORD_BOT_TOKEN')
 with open(token_file_path, "r") as token_file:
@@ -8,8 +9,23 @@ with open(token_file_path, "r") as token_file:
 
 client = commands.Bot(command_prefix=commands.when_mentioned_or('.'))
 
+async def resurrect(message):
+  if message.author == client.user:
+    return
+
+  await message.channel.send(
+    textwrap.dedent('''
+      {author}消さないで！
+      ```
+      {content}
+      ```
+    ''')
+    .format(content=message.content, author=message.author.mention)
+    .strip()
+  )
+
 @client.event
-async def on_ready():
-    print("I'm ready")
+async def on_message_delete(message):
+  await resurrect(message)
 
 client.run(TOKEN)
